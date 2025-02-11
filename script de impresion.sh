88a5def5232785294bbada99f38dc6a1b9fc6a28
Marcos Gonzalez Sanchez <marcutigon@gmail.com>
2025-02-11 09:06:04 +0100

    Add files via upload

---
+++ script de impresion.sh
@@ -0,0 +1,46 @@
+#!/bin/bash 
+
+echo "impresión [--help] [-i] [-d] [-s] [-c] [-r] [-p] [-u] [-cups]" 
+echo "" 
+echo "[--help] Muestra la ayuda" 
+echo "[-i] Instala el servicio" 
+echo "[-d] Desinstala el Servicio" 
+echo "[-s] Muestra el status del Servicio" 
+echo "[-c] Archivo de Configuracion"
+echo "[-r] Reinicia el Servicio"
+echo "[-p] Cambiar el puerto"
+echo "[-u] Configurar Firewall"
+echo "[-cups] Abre en Firefox Cups"
+echo "[-otros] Mas opciones..."
+echo ""
+
+
+
+if [ $1 = "-otros" ]; then
+    echo "Elige una Opcion:"
+    echo ""
+    echo "1- Generar Archivo para impresión"
+    echo "2- Mostrar los archivo Impresos"
+    echo "3- Conversion de PDF a WORD de los archivos impresos"
+    echo "4- Recursos que esta usando Cups"
+    echo "5- Copia de Seguridad en Zip"
+    echo ""
+fi
+
+if [ $1 = "-i" ]; then
+	sudo apt update
+	sudo apt upgrade -y
+	sudo apt install cups
+	sudo apt install cups-pdf
+	sudo usermod -a -G lpadmin $whoami
+fi
+
+if [ $1 = "-d" ]; then
+    
+
+
+if [ $1 = "-cups" ]; then
+	ip=$(hostname -I | awk '{print $1}')
+	url="http://$ip:631"
+	firefox "$url" &
+fi
